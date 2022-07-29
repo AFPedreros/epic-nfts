c2df6556ab9f5bdadfa7cb10a1a061603937c7d5
AFPedreros <felipe.pedreros94@gmail.com>
2022-07-29 17:50:36 -0500

    Final

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
   solidity: '0.8.1',
@@ -10,4 +11,9 @@ module.exports = {
       accounts: [process.env.RINKEBY_PRIVATE_KEY],
     },
   },
+    etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "FUCQJGC58DRDZTQFS2KS1J1FBHP1FBZIDA",
+  }
 };
